024062e4e075c22c0b42855abbd0d224bf1a787e
Lakshmi <lakshmiynh8@gmail.com>
2024-03-19 10:12:07 +0530

    Added:file to calculate Daily Employee wage

--- EmployeeManagement.cs
+++ EmployeeManagement.cs
@@ -12,24 +12,30 @@ namespace CompanyManagement
         {
 
             int Attendance = 1;
-            
 
-            
+            int onehvrwage = 20;
+            int employeeworkinghvrs = 0;
+            int employeeonedaywage = 0;
 
             Random random = new Random();
 
-            //computation
+            // check present or absent
             int checkAttendance = random.Next(0, 2);
 
             if (checkAttendance == Attendance)
             {
                 Console.WriteLine("Employee is present");
-                
+                employeeworkinghvrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                employeeworkinghvrs = 0;
             }
+
+            employeeonedaywage=employeeworkinghvrs*onehvrwage;
+            Console.WriteLine("Employee wage="+employeeonedaywage);
+
         }
     }
 }
