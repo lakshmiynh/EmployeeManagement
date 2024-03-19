b8a1293a120fcc9004bea45aa6826ca5471d2841
Lakshmi <lakshmiynh8@gmail.com>
2024-03-19 09:55:39 +0530

    Added:file check employee is present or absent

---
+++ EmployeeManagement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagement
+{
+    class EmployeeManagement
+    {
+        static void Main(String[] args)
+        {
+
+            int Attendance = 1;
+            
+
+            
+
+            Random random = new Random();
+
+            //computation
+            int checkAttendance = random.Next(0, 2);
+
+            if (checkAttendance == Attendance)
+            {
+                Console.WriteLine("Employee is present");
+                
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
